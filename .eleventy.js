41c9bdb49c13cdb9e2a44c27f05beda4d8003bce
dara-icf <dara.pressley@icfnext.com>
2024-02-08 13:07:06 -0800

    add assets for netlify build

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,7 @@ const yaml = require("js-yaml");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({"src/assets/siteimg": "assets/siteimg"});
-  eleventyConfig.addPassthroughCopy({"src/assets/img": "assets/img"});
-  eleventyConfig.addPassthroughCopy({"src/assets/css/styles.css": "assets/css/styles.css"});
+  eleventyConfig.addPassthroughCopy({"src/assets": "assets"});
 
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 
